2b7f7bbea5c8600f96f75f17617e12e64fc0069a
Mohammad Anas <53580629+anasthecoder786@users.noreply.github.com>
2024-02-18 19:09:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,4 @@
 package com.driver;
-class Product
-{
-  public int product(int x, int y) {return x+y;}
-  public int product(int x, int y, int z) {return x+y+z;}
-  public double product(double x, double y) {return x+y;} 
-}
 public class Main {
   public static void main(String[] args)
   {
@@ -13,4 +7,10 @@ public class Main {
     p.product(4,5,6);
     p.product(7.2,8.4);
   }
+  public static class Product
+{
+  public int product(int x, int y) {return x+y;}
+  public int product(int x, int y, int z) {return x+y+z;}
+  public double product(double x, double y) {return x+y;} 
+}
 }
