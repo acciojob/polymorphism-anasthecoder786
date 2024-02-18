1cf2bbc8467d6f0a678e8949f04515d5bf07b211
Mohammad Anas <53580629+anasthecoder786@users.noreply.github.com>
2024-02-18 19:12:11 +0530

    Polymorphism final solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,8 +9,8 @@ public class Main {
   }
   public static class Product
 {
-  public int product(int x, int y) {return x+y;}
-  public int product(int x, int y, int z) {return x+y+z;}
-  public double product(double x, double y) {return x+y;} 
+  public int product(int x, int y) {return x*y;}
+  public int product(int x, int y, int z) {return x*y*z;}
+  public double product(double x, double y) {return x*y;} 
 }
 }
